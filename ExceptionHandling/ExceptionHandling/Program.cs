568967fed0b24cea70d78f38ae811b263757c563
Sai Deekshith <msaideekshith@gmail.com>
2020-03-27 07:43:02 +0530

    used exception handing while dividing two numbers

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 9;
+            double div = 0;
+            //trying to divide two numbers
+            try
+            {
+                div = y / x;
+            }
+            //if try fails catches an exception
+            catch (Exception e)
+            {
+                Console.WriteLine("Error is :" + e.Message);
+            }
+            //finally must execute
+            finally
+            {
+                Console.WriteLine("Result is : " + div);
+            }
+        }
+    }
+}
